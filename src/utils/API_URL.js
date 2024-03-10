ff6ad3848705d02fce5726598b02700403e82e77
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-09 21:44:27 -0400

    added source data as api material, just in case.

---
+++ src/utils/API_URL.js
@@ -0,0 +1,16 @@
+export const WIZARD_CLERIC_CANTRIPS = "https://api.open5e.com/v1/spells/?casting_time=&casting_time__iexact=&casting_time__in=&components=&concentration=&dnd_class=&dnd_class__icontains=&dnd_class__iexact=&dnd_class__in=&document__slug=&document__slug__iexact=&document__slug__in=&document__slug__not_in=&duration=&duration__iexact=&duration__in=&format=json&level_int=&name=&name__iexact=&requires_concentration=unknown&requires_material_components=unknown&requires_somatic_components=unknown&requires_verbal_components=unknown&school=&school__iexact=&school__in=&slug=&slug__iexact=&slug__in=&spell_level=0&spell_level__gt=&spell_level__gte=&spell_level__lt=&spell_level__lte=&spell_level__range=&spell_lists=wizard&spell_lists=cleric&spell_lists_not=&target_range_sort=&target_range_sort__gt=&target_range_sort__gte=&target_range_sort__lt=&target_range_sort__lte=&target_range_sort__range="
+
+export const WIZARD_CLERIC_LV1_SPELLS = "https://api.open5e.com/v1/spells/?casting_time=&casting_time__iexact=&casting_time__in=&components=&concentration=&dnd_class=&dnd_class__icontains=&dnd_class__iexact=&dnd_class__in=&document__slug=&document__slug__iexact=&document__slug__in=&document__slug__not_in=&duration=&duration__iexact=&duration__in=&format=json&level_int=&name=&name__iexact=&requires_concentration=unknown&requires_material_components=unknown&requires_somatic_components=unknown&requires_verbal_components=unknown&school=&school__iexact=&school__in=&slug=&slug__iexact=&slug__in=&spell_level=1&spell_level__gt=&spell_level__gte=&spell_level__lt=&spell_level__lte=&spell_level__range=&spell_lists=wizard&spell_lists=cleric&spell_lists_not=&target_range_sort=&target_range_sort__gt=&target_range_sort__gte=&target_range_sort__lt=&target_range_sort__lte=&target_range_sort__range="
+
+
+
+// Full Database items below, for future reference.
+export const SPELLS = "https://api.open5e.com/v1/spells/?format=json"
+export const SPELL_LIST = "https://api.open5e.com/v1/spelllist/?format=json"
+export const BACKGROUNDS = "https://api.open5e.com/v1/backgrounds/?format=json"
+export const FEATS = "https://api.open5e.com/v1/feats/?format=json"
+export const RACES = "https://api.open5e.com/v1/races/?format=json"
+export const CLASS = "https://api.open5e.com/v1/classes/?format=json"
+export const MAGIC_ITEMS = "https://api.open5e.com/v1/magicitems/?format=json"
+export const WEAPONS = "https://api.open5e.com/v1/weapons/?format=json"
+export const ARMOR = "https://api.open5e.com/v1/armor/?format=json"
