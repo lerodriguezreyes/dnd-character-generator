742ebcb72284d9cb0bc714368bdb2671a6dc56e7
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-10 22:49:37 -0400

    Erased unneeded images. moved image arrays for memes and stories into the  randomizer util

--- src/pages/LandingScreen.jsx
+++ src/pages/LandingScreen.jsx
@@ -1,30 +1,6 @@
 import { diceRoll } from "../utils/randomizer";
+import { dndMemesArray, storiesImagesArray } from "../utils/randomizer";
 
-const storiesImagesArray = [
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience1.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience2.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience3.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience4.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience5.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience6.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience7.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience8.png?raw=true",
-  "https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/experience9.png?raw=true",
-];
-
-const dndMemesArray = [
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/atreyu.png?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/barbarianStealth.png?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/binks.jpeg?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/cowabunga.png?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/dm.png?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/longRest.jpeg?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/party.jpg?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/pyramidScheme.jpg?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/smartBarb.jpg?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/unconfortableOrc.png?raw=true',
-  'https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/wizardFireball.png?raw=true'
-]
 
 function LandingScreen() {
   return (
