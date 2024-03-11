6365877a100f8d050626d245f22549e04c8e365a
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-10 23:49:52 -0400

    added code from previous project for adding character and deleting character. Also moved up the image generation requester.

--- src/context/generator.context.jsx
+++ src/context/generator.context.jsx
@@ -8,6 +8,38 @@ const GeneratorContext = createContext()
 const ContextProvider = ({ children }) => {
 
     const [characters, setCharacters] = useState([])
+   
+    // const addNewCharacter = (newCharacter) => {
+    //     newTask.id = String(tasksIds)
+    //     let newTasks = [newTask, ...tasks]
+    //     setTaskIds((prev) => prev + 1)
+    //     setTasks(newTasks)
+    //   }
+    
+    //   const deleteTask = (taskId) => {
+    //     const filteredTasks = tasks.filter((task) => {
+    //       return task.id !== taskId;
+    //     })
+    //     setTasks(filteredTasks);
+    //   }
+
+    // DALL-E request
+    // POST https://api.openai.com/v1/images/generations
+    // const response = await openai.createImage({
+    //     model: "dall-e-3",
+    //     prompt: "a white siamese cat",
+    //     n: 1,
+    //     quality: 'hd'
+    //     size: "1024x1024",
+    //     style: 'vivid'
+    //   });
+    //   image_url = response.data.data[0].url;
+
+
+
+
+
+
 
     useEffect (() => {
         axios.get(BACKEND_URL + '/characters')
@@ -33,14 +65,3 @@ export {GeneratorContext, ContextProvider}
 
 
 
-// DALL-E command
-// POST https://api.openai.com/v1/images/generations
-// const response = await openai.createImage({
-//     model: "dall-e-3",
-//     prompt: "a white siamese cat",
-//     n: 1,
-//     quality: 'hd'
-//     size: "1024x1024",
-//     style: 'vivid'
-//   });
-//   image_url = response.data.data[0].url;
