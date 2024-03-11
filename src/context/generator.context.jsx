d98f855fe79a5a0cf3ac0bd6af2ce1f2d874207c
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-10 22:47:54 -0400

    added context in state variable for characters data

--- src/context/generator.context.jsx
+++ src/context/generator.context.jsx
@@ -1,7 +1,46 @@
-import { createContext, useEffect, useState } from `react`;
+import { createContext, useEffect, useState } from 'react';
 
-import { BACKEND_URL } from `../utils/API_URL`;
+import { BACKEND_URL } from '../utils/BACKEND_API';
 
-import axios from `axios`
+import axios from 'axios';
 
 const GeneratorContext = createContext()
+const ContextProvider = ({ children }) => {
+
+    const [characters, setCharacters] = useState([])
+
+    useEffect (() => {
+        axios.get(BACKEND_URL + '/characters')
+            .then((response) => {
+                console.log('Characters object =====>', response.data)
+                setCharacters(response.data)
+            })
+            .catch((err) => {console.log(err) 
+            })
+            
+            }, [])
+    return (
+        <GeneratorContext.Provider value={{ characters, setCharacters }}>
+            
+            {children}
+
+        </GeneratorContext.Provider>
+    )
+
+}
+
+export {GeneratorContext, ContextProvider}
+
+
+
+// DALL-E command
+// POST https://api.openai.com/v1/images/generations
+// const response = await openai.createImage({
+//     model: "dall-e-3",
+//     prompt: "a white siamese cat",
+//     n: 1,
+//     quality: 'hd'
+//     size: "1024x1024",
+//     style: 'vivid'
+//   });
+//   image_url = response.data.data[0].url;
