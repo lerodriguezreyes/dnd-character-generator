16c1d9917d51a94bc03cd48be20ac7b6fce4b925
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-12 13:03:24 -0400

    enabled deletebutton

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,8 +1,10 @@
 import { useContext, useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
-
+import { GeneratorContext } from "../context/generator.context"
 
 function CharacterCard({ character }) {
+  const {deleteCharacter } = useContext(GeneratorContext)
+
   return (
     <div key={character.id} className="characterCard">
     <h4>{character.name}</h4>
@@ -10,7 +12,7 @@ function CharacterCard({ character }) {
     <p>Class: {character.class} </p>
     <p>Level: {character.level} </p>
     <p>Current Campaign: {character.currentCampaign} </p>
-    <p></p>
+    <button onClick={() => deleteCharacter(character.id)}>Delete</button>
     </div>
   )
 }
