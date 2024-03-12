8fc886144a4e7ab0106c43fc7608f1d6e147e16b
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-12 13:05:11 -0400

    started form with event handlers.

--- src/context/generator.context.jsx
+++ src/context/generator.context.jsx
@@ -7,52 +7,119 @@ import axios from 'axios';
 const GeneratorContext = createContext()
 const ContextProvider = ({ children }) => {
 
-    const [characters, setCharacters] = useState([])
-   
+    const [charactersData, setCharactersData] = useState([])
+    const [racesData, setRacesData] = useState([])
+    const [classesData, setClassesData] = useState([])
+    const [backgroundsData, setBackgroundsData] = useState([])
+    const [cantripsData, setCantripsData] = useState([])
+    const [lv1SpellsData, setLv1SpellsData] = useState([])
+    const [weaponsData, setWeaponsData] = useState([])
+    const [armorsData, setArmorsData] = useState([])
+
     // const addNewCharacter = (newCharacter) => {
-    //     newTask.id = String(tasksIds)
-    //     let newTasks = [newTask, ...tasks]
-    //     setTaskIds((prev) => prev + 1)
-    //     setTasks(newTasks)
+    //     newCharacter.id = String(characterIds)
+    //     let newCharacters = [newCharacter, ...characters]
+    //     setCharacterIds((prev) => prev + 1)
+    //     setCharacters(newCharacters)
     //   }
     
-    //   const deleteTask = (taskId) => {
-    //     const filteredTasks = tasks.filter((task) => {
-    //       return task.id !== taskId;
-    //     })
-    //     setTasks(filteredTasks);
-    //   }
-
-    // DALL-E request
-    // POST https://api.openai.com/v1/images/generations
-    // const response = await openai.createImage({
-    //     model: "dall-e-3",
-    //     prompt: "a white siamese cat",
-    //     n: 1,
-    //     quality: 'hd'
-    //     size: "1024x1024",
-    //     style: 'vivid'
-    //   });
-    //   image_url = response.data.data[0].url;
-
-
-
-
-
+    const deleteCharacter = (characterId) => {
+        const filteredCharacters = characters.filter((character) => {
+          return character.id !== characterId;
+        })
+        setCharacters(filteredCharacters);
+      }
 
 
     useEffect (() => {
-        axios.get(BACKEND_URL + '/characters')
+        axios.get(BACKEND_URL + "characters")
             .then((response) => {
-                console.log('Characters object =====>', response.data)
-                setCharacters(response.data)
+                console.log('API character data object ====>', response.data)
+                setCharactersData(response.data)
             })
             .catch((err) => {console.log(err) 
             })
             
             }, [])
+
+    useEffect (() => {
+            axios.get(BACKEND_URL + "races")
+                .then((response) => {
+                    console.log('API races data object ====>', response.data)
+                    setRacesData(response.data)
+                })
+                .catch((err) => {console.log(err) 
+                })
+                
+                }, [])
+
+    useEffect (() => {
+            axios.get(BACKEND_URL + "classes")
+                .then((response) => {
+                    console.log('API classes data object ====>', response.data)
+                    setClassesData(response.data)
+                })
+                .catch((err) => {console.log(err) 
+                })
+                
+                }, [])
+
+    useEffect (() => {
+        axios.get(BACKEND_URL + "backgrounds")
+            .then((response) => {
+                console.log('API backgrounds data object ====>', response.data)
+                setBackgroundsData(response.data)
+            })
+            .catch((err) => {console.log(err) 
+            })
+            
+            }, [])
+
+    useEffect (() => {
+        axios.get(BACKEND_URL + "cantrips")
+            .then((response) => {
+                console.log('API cantrips data object ====>', response.data)
+                setCantripsData(response.data)
+            })
+            .catch((err) => {console.log(err) 
+            })
+            
+            }, [])
+
+    useEffect (() => {
+        axios.get(BACKEND_URL + "lv1spells")
+            .then((response) => {
+                console.log('API lv1spells data object ====>', response.data)
+                setCantripsData(response.data)
+            })
+            .catch((err) => {console.log(err) 
+            })
+            
+            }, [])
+    useEffect (() => {
+        axios.get(BACKEND_URL + "weapons")
+            .then((response) => {
+                console.log('API weapons data object ====>', response.data)
+                setCantripsData(response.data)
+            })
+            .catch((err) => {console.log(err) 
+            })
+            
+            }, [])
+
+    useEffect (() => {
+        axios.get(BACKEND_URL + "armors")
+            .then((response) => {
+                console.log('API weapons data object ====>', response.data)
+                setArmorsData(response.data)
+            })
+            .catch((err) => {console.log(err) 
+            })
+            
+            }, [])
+            
     return (
-        <GeneratorContext.Provider value={{ characters, setCharacters }}>
+        <GeneratorContext.Provider value={{ charactersData, setCharactersData, deleteCharacter, racesData, setRacesData, classesData, setClassesData, backgroundsData, setBackgroundsData, cantripsData, setCantripsData, lv1SpellsData, setLv1SpellsData, weaponsData, setWeaponsData, armorsData, setArmorsData }}>
             
             {children}
 
