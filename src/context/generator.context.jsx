ff39346b3cc50ff880024672f3c6c7c51491d859
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-13 15:30:17 -0400

    fixed create axios post

--- src/context/generator.context.jsx
+++ src/context/generator.context.jsx
@@ -1,31 +1,30 @@
 import { createContext, useEffect, useState } from 'react';
-
 import { BACKEND_URL } from '../utils/BACKEND_API';
-
 import axios from 'axios';
+
 
 const GeneratorContext = createContext()
 const ContextProvider = ({ children }) => {
 
     const [charactersData, setCharactersData] = useState([])
     const [characterIds, setCharacterIds ] = useState([])
-useEffect (() => {
+    useEffect (() => {
         axios.get(BACKEND_URL + "characters")
             .then((response) => {
                 console.log('API character data object ====>', response.data)
                 setCharactersData(response.data)
             })
-            .catch((err) => {console.log(err) 
+        .catch((err) => {console.log(err) 
             })
-            
-            }, [])
+        
+        }, [])
 
     const addNewCharacter = (newCharacter) => {
         newCharacter.id = String(characterIds)
         let newCharacters = [newCharacter, ...charactersData]
         setCharacterIds((prev) => prev + 1)
         setCharactersData(newCharacters)
-        axios.post(BACKEND_URL + 'characters')
+        axios.post(BACKEND_URL + 'characters', newCharacter)
             .then((response) => {
                 console.log('New Character added ===>', response.data)
                 })
@@ -46,8 +45,6 @@ useEffect (() => {
           return character.id !== characterId;
         })
         setCharactersData(filteredCharacters);
-        
-       
       }
 
 
