c1b8b629bc7ad4a854014eec1b87ace380b61956
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-12 22:40:19 -0400

    finished survey - does not let me submit, states type error.

--- src/context/generator.context.jsx
+++ src/context/generator.context.jsx
@@ -8,30 +8,8 @@ const GeneratorContext = createContext()
 const ContextProvider = ({ children }) => {
 
     const [charactersData, setCharactersData] = useState([])
-    const [racesData, setRacesData] = useState([])
-    const [classesData, setClassesData] = useState([])
-    const [backgroundsData, setBackgroundsData] = useState([])
-    const [cantripsData, setCantripsData] = useState([])
-    const [lv1SpellsData, setLv1SpellsData] = useState([])
-    const [weaponsData, setWeaponsData] = useState([])
-    const [armorsData, setArmorsData] = useState([])
-
-    // const addNewCharacter = (newCharacter) => {
-    //     newCharacter.id = String(characterIds)
-    //     let newCharacters = [newCharacter, ...characters]
-    //     setCharacterIds((prev) => prev + 1)
-    //     setCharacters(newCharacters)
-    //   }
-    
-    const deleteCharacter = (characterId) => {
-        const filteredCharacters = characters.filter((character) => {
-          return character.id !== characterId;
-        })
-        setCharacters(filteredCharacters);
-      }
-
-
-    useEffect (() => {
+    const [characterIds, setCharacterIds ] = useState([])
+useEffect (() => {
         axios.get(BACKEND_URL + "characters")
             .then((response) => {
                 console.log('API character data object ====>', response.data)
@@ -42,84 +20,31 @@ const ContextProvider = ({ children }) => {
             
             }, [])
 
-    useEffect (() => {
-            axios.get(BACKEND_URL + "races")
-                .then((response) => {
-                    console.log('API races data object ====>', response.data)
-                    setRacesData(response.data)
+    const addNewCharacter = (newCharacter) => {
+        newCharacter.id = String(characterIds)
+        let newCharacters = [newCharacter, ...characters]
+        setCharacterIds((prev) => prev + 1)
+        setCharactersData(newCharacters)
+        axios.post(BACKEND_URL + 'characters')
+            .then((response) => {
+                console.log('New Character added ===>', response.data)
                 })
-                .catch((err) => {console.log(err) 
-                })
-                
-                }, [])
+            .catch((error) => {
+                console.log(error)
+            }) 
+      }
+    
+    const deleteCharacter = (characterId) => {
+        const filteredCharacters = characters.filter((character) => {
+          return character.id !== characterId;
+        })
+        setCharacters(filteredCharacters);
+      }
 
-    useEffect (() => {
-            axios.get(BACKEND_URL + "classes")
-                .then((response) => {
-                    console.log('API classes data object ====>', response.data)
-                    setClassesData(response.data)
-                })
-                .catch((err) => {console.log(err) 
-                })
-                
-                }, [])
 
-    useEffect (() => {
-        axios.get(BACKEND_URL + "backgrounds")
-            .then((response) => {
-                console.log('API backgrounds data object ====>', response.data)
-                setBackgroundsData(response.data)
-            })
-            .catch((err) => {console.log(err) 
-            })
-            
-            }, [])
-
-    useEffect (() => {
-        axios.get(BACKEND_URL + "cantrips")
-            .then((response) => {
-                console.log('API cantrips data object ====>', response.data)
-                setCantripsData(response.data)
-            })
-            .catch((err) => {console.log(err) 
-            })
-            
-            }, [])
-
-    useEffect (() => {
-        axios.get(BACKEND_URL + "lv1spells")
-            .then((response) => {
-                console.log('API lv1spells data object ====>', response.data)
-                setCantripsData(response.data)
-            })
-            .catch((err) => {console.log(err) 
-            })
-            
-            }, [])
-    useEffect (() => {
-        axios.get(BACKEND_URL + "weapons")
-            .then((response) => {
-                console.log('API weapons data object ====>', response.data)
-                setCantripsData(response.data)
-            })
-            .catch((err) => {console.log(err) 
-            })
-            
-            }, [])
-
-    useEffect (() => {
-        axios.get(BACKEND_URL + "armors")
-            .then((response) => {
-                console.log('API weapons data object ====>', response.data)
-                setArmorsData(response.data)
-            })
-            .catch((err) => {console.log(err) 
-            })
-            
-            }, [])
-            
-    return (
-        <GeneratorContext.Provider value={{ charactersData, setCharactersData, deleteCharacter, racesData, setRacesData, classesData, setClassesData, backgroundsData, setBackgroundsData, cantripsData, setCantripsData, lv1SpellsData, setLv1SpellsData, weaponsData, setWeaponsData, armorsData, setArmorsData }}>
+    
+       return (
+        <GeneratorContext.Provider value={{ charactersData, setCharactersData, addNewCharacter, deleteCharacter,}}>
             
             {children}
 
