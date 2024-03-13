a535e76c417d87fa0a7c9a60ca90bdbcf36c24df
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-13 15:28:51 -0400

    created actions html component

---
+++ src/components/ActionsList.jsx
@@ -0,0 +1,43 @@
+function ActionsList() {
+  return (
+    <div className="actionsList">
+      <p> As Movement...</p>
+      <ul>
+        <li>Move</li>
+        <li>Climb</li>
+        <li>Swim</li>
+        <li>Drop Prone</li>
+        <li>Crawl</li>
+        <li>Stand Up</li>
+        <li>High Jump</li>
+        <li>Long Jump</li>
+      </ul>
+      <p> As an Action...</p>
+      <ul>
+        <li>Attack</li>
+        <li>Grapple</li>
+        <li>Shove</li>
+        <li>Cast Spell</li>
+        <li>Use Class Feature</li>
+        <li>Dash</li>
+        <li>Help</li>
+        <li>Search</li>
+        <li>Use object</li>
+        <li>Ready Action</li>
+        <li>Dodge</li>
+        <li>Disengage</li>
+        <li>Hide</li>
+      </ul>
+      <p> As a bonus action or reaction...</p>
+      <ul>
+        <li>Offhand Attack</li>
+        <li>Cast a Spell</li>
+        <li>Use Class Feature</li>
+        <li>Opportunity Attack</li>
+        <li>Activate Ready Action</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ActionsList;
