2f810f9bcb656464bc977032fb4bf396f28b8de2
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-13 15:29:50 -0400

    created toast structure

---
+++ src/components/DeleteToast.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function deleteToast() {
+  return (
+    <div className='toast'>
+      <img src='https://github.com/lerodriguezreyes/dnd-character-generator/blob/main/src/assets/toast.png?raw=true' style={{height: "200 px" }} />
+      <p>Character succesfully deleted!</p>
+    </div>
+  )
+}
+
+export default deleteToast
