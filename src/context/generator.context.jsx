498c7a7e7b8bb077022c7a8e624fa49498d87b5e
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-14 16:09:46 -0400

    ok

--- src/context/generator.context.jsx
+++ src/context/generator.context.jsx
@@ -9,50 +9,45 @@ const ContextProvider = ({ children }) => {
   const [charactersData, setCharactersData] = useState([]);
   const [characterIds, setCharacterIds] = useState(4);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const getCharacters = () => {
     axios
-    .get(BACKEND_URL + "characters")
-    .then((response) => {
-      console.log("API character data object ====>", response.data);
-      setCharactersData(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
+      .get(BACKEND_URL + "characters")
+      .then((response) => {
+        console.log("API character data object ====>", response.data);
+        setCharactersData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-    getCharacters()
+    getCharacters();
   }, []);
 
   const addNewCharacter = (newCharacter) => {
-    // newCharacter.id = String(characterIds);
-
-    newCharacter.textToImagePrompt = `Photorealistic portrait of a ${newCharacter.race} ${newCharacter.skinColor}-skinned ${newCharacter.sex} ${newCharacter.job} dungeons and dragons character that has ${newCharacter.eyeColor} eyes, ${newCharacter.hairColor} colored hair ${newCharacter.hairStlye} styled, wearing ${newCharacter.armor} and wielding a ${newCharacter.mainWeapon}.`
+    newCharacter.textToImagePrompt = `Photorealistic portrait of a ${newCharacter.race} ${newCharacter.skinColor}-skinned ${newCharacter.sex} ${newCharacter.job} dungeons and dragons character that has ${newCharacter.eyeColor} eyes, ${newCharacter.hairColor} colored hair ${newCharacter.hairStlye} styled, wearing ${newCharacter.armor} and wielding a ${newCharacter.mainWeapon}.`;
     imageGenerator(newCharacter.textToImagePrompt)
       .then((url) => {
-        newCharacter.image = url
+        newCharacter.image = url;
         axios
           .post(BACKEND_URL + "characters", newCharacter)
           .then((response) => {
             console.log("New Character added ===>", response.data);
-            setCharactersData([...charactersData, response.data])
+            setCharactersData([...charactersData, response.data]);
           })
           .catch((error) => {
             console.log(error);
           })
           .finally(() => {
-            navigate('/character')
-          })
+            navigate("/character");
+          });
       })
       .catch((err) => {
-        console.log(err)
-      })
-    // let newCharacters = [newCharacter, ...charactersData];
-    // // setCharacterIds((prev) => prev + 1);
-    // setCharactersData(newCharacters);
+        console.log(err);
+      });
   };
 
   const deleteCharacter = (characterId) => {
@@ -77,7 +72,7 @@ const ContextProvider = ({ children }) => {
         setCharactersData,
         addNewCharacter,
         deleteCharacter,
-        getCharacters
+        getCharacters,
       }}
     >
       {children}
