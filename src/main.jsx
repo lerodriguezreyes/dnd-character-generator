5a2411ebfbe1d01d8139464a22eb605c9e1a74bf
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-14 13:22:47 -0400

    new image generated by dalle 3!

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./context/generator.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
     <BrowserRouter>
       <ContextProvider>
         <App />
       </ContextProvider>
     </BrowserRouter>
-  </React.StrictMode>
+
 );
