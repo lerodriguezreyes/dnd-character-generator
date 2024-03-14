1bbb0d4baf82dfb0874ff0f7bfb6d70072370dda
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-14 17:13:08 -0400

    changed url for backend

--- src/utils/BACKEND_API.js
+++ src/utils/BACKEND_API.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = 'http://localhost:4000/'
+export const BACKEND_URL = 'https://mockserverjasondndgen.adaptable.app/'
+
+// bottom one is for local stuff!
+// export const BACKEND_URL = 'http://localhost:4000/'
