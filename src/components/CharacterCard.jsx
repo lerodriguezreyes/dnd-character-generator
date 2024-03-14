04cd232551010fb0bba57a5fe5cc5a12ba435d90
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-14 16:09:16 -0400

    started  css work

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,16 +1,15 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { GeneratorContext } from "../context/generator.context";
+import '../styles/CharacterCard.css'
 
 function CharacterCard({ character }) {
   const { deleteCharacter } = useContext(GeneratorContext);
 
   return (
     <div key={character.id} className="characterCard">
-      <h4>
-        <Link to={`/detail/${character.id}`}>{character.name}</Link>
-      </h4>
-      <img src={character.image} />
+      <img className="profile" src={character.image} />
+      <h2><Link to={`/detail/${character.id}`}>{character.name}</Link></h2>
       <p>Class: {character.job} </p>
       <p>Level: {character.lv} </p>
       <p>Current Campaign: {character.campaign} </p>
