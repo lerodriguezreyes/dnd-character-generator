a512e5ee49bcabc35d9d3228dc8697942c406d4c
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-03-14 18:22:05 -0400

    move open ai to server

--- src/utils/OPENAI.js
+++ src/utils/OPENAI.js
@@ -1,43 +1,35 @@
-// import { Configuration, OpenAIApi} from 'openai'
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_MY_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
-// const configuration= new Configuration({
-//   apiKey: import.meta.env.VITE_MY_API_KEY,
-
-// })
-
-// const openai = new OpenAIApi(configuration);
-
+// // import { Configuration, OpenAIApi} from 'openai'
 // import OpenAI from "openai";
+import axios from "axios";
+// const openai = new OpenAI({
+//   apiKey: import.meta.env.VITE_MY_API_KEY,
+//   dangerouslyAllowBrowser: true,
+// });
+// // const configuration= new Configuration({
+// //   apiKey: import.meta.env.VITE_MY_API_KEY,
 
-// const openai = new OpenAI();
+// // })
 
-// async function main() {
-//   const image = await openai.images.generate({ model: "dall-e-3", prompt: "A cute baby sea otter" });
+// // const openai = new OpenAIApi(configuration);
 
-//   console.log(image.data);
-// }
-// main();
+// // import OpenAI from "openai";
 
+// // const openai = new OpenAI();
 
+// // async function main() {
+// //   const image = await openai.images.generate({ model: "dall-e-3", prompt: "A cute baby sea otter" });
 
+// //   console.log(image.data);
+// // }
+// // main();
 
 export const imageGenerator = async (string) => {
-    try {
-      const response = await openai.images.generate({
-        model: "dall-e-3",
-        prompt: `${string}`,
-        n: 1,
-        size: '1024x1024',
-    })
-    console.log("This is all of OpenAI response", response.data[0].url)
-    console.log('This is the data ==>', response.data[0].url)
-    return response.data[0].url
-    } catch(error) {console.log(error)
-    }
+  try {
+    console.log(string)
+    const response = await axios.post("http://localhost:4000/ai/image-generator", {prompt:string});
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
   }
-
+};
